0ffd8edf5d60e7c878f3c68adaaee723eb571934
Marina Balioura <mettta@gmail.com>
2021-04-01 11:27:48 +0200

    add TODO to DOM

--- src/DOM.js
+++ src/DOM.js
@@ -21,6 +21,15 @@ export default class DocumentObjectModel {
   _setAttribute(element, selector) {
     element.setAttribute(selector.substring(1, selector.length - 1), '');
   }
+  // todo ^^^
+  // if (!String.prototype.trim) {
+  //   (function() {
+  //     // Вырезаем BOM и неразрывный пробел
+  //     String.prototype.trim = function() {
+  //       return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+  //     };
+  //   })();
+  // }
 
   setPrintIgnore(element) {
     // element.dataset.printIgnore = '';
