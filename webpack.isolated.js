d9b5a4e8b920a3ed9bdf649d9cb00d31fb1c1565
Marina Balioura <mettta@gmail.com>
2021-06-20 05:17:59 +0200

    webpack: add Isolated example

---
+++ webpack.isolated.js
@@ -0,0 +1,19 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      appMountId: 'app',
+      title: '&star; isolated',
+      filename: 'isolated.html',
+      template: './examples/isolated.html',
+    })
+  ],
+});
