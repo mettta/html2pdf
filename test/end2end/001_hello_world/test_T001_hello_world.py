1a70e94f971df2e05e1b08f11340a087ccc14835
Maryna Balioura <mettta@gmail.com>
2023-05-22 19:00:21 +0200

    test/end2end: add helpers

--- test/end2end/001_hello_world/test_T001_hello_world.py
+++ test/end2end/001_hello_world/test_T001_hello_world.py
@@ -2,16 +2,14 @@ import os
 
 from seleniumbase import BaseCase
 
-path_to_this_test_file_folder = os.path.dirname(os.path.abspath(__file__))
+from test.end2end.helpers.helper import Helper
 
+path_to_this_test_file_folder = os.path.dirname(os.path.abspath(__file__))
+index_html_file_url = (
+    "file:///" + os.path.join(path_to_this_test_file_folder, "index.html")
+)
 
 class Test_T001_HelloWorld(BaseCase):
     def test_001(self):
-        index_html_file_url = (
-            "file:///" +
-            os.path.join(path_to_this_test_file_folder, "index.html")
-        )
-        self.open(index_html_file_url)
-        self.assert_text("Hello world!")
-        self.assert_no_404_errors()
-        self.assert_no_js_errors()
+        helper = Helper(self)
+        helper.do_open_and_assert(index_html_file_url, "Hello world!")
--- test/end2end/010_second_page/test_T010_second_page.py
+++ test/end2end/010_second_page/test_T010_second_page.py
@@ -3,53 +3,21 @@ import os
 from selenium.webdriver.common.by import By
 from seleniumbase import BaseCase
 
-path_to_this_test_file_folder = os.path.dirname(os.path.abspath(__file__))
+from test.end2end.helpers.helper import Helper
 
+path_to_this_test_file_folder = os.path.dirname(os.path.abspath(__file__))
+index_html_file_url = (
+    "file:///" + os.path.join(path_to_this_test_file_folder, "index.html")
+)
 test_text = "I want to be on page 2."
 
 class Test_T010_SecondPage(BaseCase):
     def test_01(self):
-        index_html_file_url = (
-            "file:///" +
-            os.path.join(path_to_this_test_file_folder, "index.html")
-        )
-        self.open(index_html_file_url)
-        self.assert_no_404_errors()
-        self.assert_no_js_errors()
+        helper= Helper(self)
+        helper.do_open_and_assert(index_html_file_url, test_text)
 
-        self.assert_text(test_text)
-        self.assert_element(
-            '(//*[@id="paperFlow"]/*[@class="virtualPaper"])[2]',
-            by=By.XPATH,
-        )
-        self.assert_element_not_present(
-            '(//*[@id="paperFlow"]/*[@class="virtualPaper"])[3]',
-            by=By.XPATH,
-        )
-
-        # print(dir(element))
-        # print(element.location)
-
-        # Check that the Test object is shifted to the second page.
-        # That is, it is lower than the top of the second page.
-        test_object = self.find_element(
-            f'//*[@id="contentFlow"]//*[contains(., "{test_text}")]',
-            by=By.XPATH,
-        )
-        # print(test_object.location)
-        page_2_top_point = self.find_element(
-            '//*[@id="contentFlow"]//*[@class="virtualPaperTopMargin"][2]',
-            by=By.XPATH,
-        )
-        # print(page_2_top_point.location)
-        assert page_2_top_point.location["y"] < test_object.location["y"]
-
-        # Check that two pages are rendered
-        all_papers = self.find_elements(
-            '(//*[@id="paperFlow"]/*[@class="virtualPaper"])',
-            by=By.XPATH,
-        )
-        assert len(all_papers) == 2
+        helper.assert_document_has_pages(2, True)
+        helper.assert_element_on_the_page(f'//*[contains(., "{test_text}")]', 2)
 
         # /*[@data-content-flow-end]
 
