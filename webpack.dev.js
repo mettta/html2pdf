25831523ec07c850875bd1fe07e073199830804c
Maryna Balioura <mettta@gmail.com>
2023-05-25 17:22:24 +0200

    webpack: use 'html-webpack-inject-attributes-plugin' to inject config into script tag with data-attribute

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const htmlWebpackInjectAttributesPlugin = require('html-webpack-inject-attributes-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
@@ -11,21 +12,23 @@ module.exports = merge(common, {
   plugins: [
     new HtmlWebpackPlugin({
       appMountId: 'app',
-      title: '&star; isolated',
+      filename: 'index.html',
+      template: './examples/index.html',
+      attributes: {
+        'data-src': function (tag) { return tag.attributes.src },
+        'config': '{printLeftMargin: 40,}',
+      },
+    }),
+    new HtmlWebpackPlugin({
+      appMountId: 'app',
       filename: 'isolated.html',
       template: './examples/isolated.html',
     }),
     new HtmlWebpackPlugin({
       appMountId: 'app',
-      title: '&star; simple',
-      filename: 'simple.html',
-      template: './examples/simple.html',
-    }),
-    new HtmlWebpackPlugin({
-      appMountId: 'app',
-      title: '&star; STRICTDOC',
       filename: 'strictdoc.html',
       template: './examples/strictdoc.html',
-    })
+    }),
+    new htmlWebpackInjectAttributesPlugin()
   ],
 });
