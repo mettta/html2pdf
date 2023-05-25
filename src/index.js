64e5021ba4f9c8fe9151611781ee57c45b46ebce
Maryna Balioura <mettta@gmail.com>
2023-05-25 23:33:19 +0200

    src: add Preloader

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import HTML2PDF4DOC from './app';
+import Preloader from './preloader';
+
+// preloader
+const preloader = new Preloader();
+preloader.create();
 
 // test
 import _emulateContent from './content';
 
 // params
 const customConfig = document.currentScript.dataset;
-console.log(customConfig, 'document.currentScript')
 
-window.addEventListener("load", function (event) {
+// add listener
+window.addEventListener("DOMContentLoaded", function (event) {
+  console.log("on DOMContentLoaded Event");
   console.time("printTHIS");
 
   // _emulateContent();
@@ -15,5 +21,11 @@ window.addEventListener("load", function (event) {
   const app = new HTML2PDF4DOC(customConfig);
   app.render();
 
+  preloader.remove();
+
   console.timeEnd("printTHIS");
 });
+
+window.addEventListener("load", function (event) {
+  console.log("on Load Event");
+})
---
+++ src/preloader.js
@@ -0,0 +1,74 @@
+export default class Preloader {
+
+  constructor() {
+    this.preloader;
+  }
+
+  create() {
+    console.log("Preloader created");
+    this._insertStyle();
+
+    this.preloader = document.createElement('div');
+    this.preloader.classList.add('lds-dual-ring');
+    document.body.append(this.preloader);
+  }
+
+  remove() {
+    let op = 1;  // initial opacity
+
+    const fadeTimer = setInterval(() => {
+        if (op <= 0.1){
+            clearInterval(fadeTimer);
+            this.preloader.remove();
+        }
+        this.preloader.style.opacity = op;
+        op -= op * 0.1;
+    }, 50);
+
+    console.log("Preloader removed");
+  }
+
+  _insertStyle() {
+    const head = document.querySelector('head');
+    const style = document.createElement('style');
+    style.append(document.createTextNode(this._css()));
+    style.setAttribute("data-preloader-style", '');
+    head.append(style);
+  }
+
+  _css() {
+    return `
+    /* PRELOADER */
+    .lds-dual-ring {
+      position: fixed;
+      z-index: 99999;
+      top: 0; left: 0; bottom: 0; right: 0;
+      background: #fff;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    /*
+    .lds-dual-ring:after {
+      content: " ";
+      display: block;
+      width: 64px;
+      height: 64px;
+      margin: 8px;
+      border-radius: 50%;
+      border: 6px solid #eee;
+      border-color: #eee transparent #eee transparent;
+      animation: lds-dual-ring 1.2s linear infinite;
+    }
+    @keyframes lds-dual-ring {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+    */
+  `;
+  }
+}
