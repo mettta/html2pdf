66ab20242317fc5e95b9dd71d63402ed4362ac27
Maryna Balioura <mettta@gmail.com>
2024-01-22 10:20:43 +0100

    DOM: refactoring

--- src/DOM.js
+++ src/DOM.js
@@ -1,39 +1,26 @@
-import SELECTOR from './selector';
-
 export default class DocumentObjectModel {
 
   constructor({DOM, debugMode}) {
 
     // * public
-    this.body = DOM.body;
     this.document = DOM;
+    this.body = DOM.body;
 
     // * private
     this._debugMode = debugMode;
-    this._DOM = DOM;
     this._debugToggler = {
       _DOM: false,
     }
   }
 
+  // CREATE ELEMENTS
+
+  createElement(selector) {
+    return this.document.createElement(selector);
+  }
+
   createDocumentFragment() {
-    return this._DOM.createDocumentFragment()
-  }
-
-  getElement(selector, target = this._DOM) {
-    return target.querySelector(selector);
-  }
-
-  getAllElements(selector, target = this._DOM) {
-    return target.querySelectorAll(selector);
-  }
-
-  getElementById(id, target = this._DOM) {
-    return target.getElementById(id);
-  }
-
-  removeNode(element) {
-    element.remove();
+    return this.document.createDocumentFragment()
   }
 
   cloneNode(node) {
@@ -44,7 +31,7 @@ export default class DocumentObjectModel {
     return node?.cloneNode(false);
   }
 
-
+  // INSERT
 
   insertBefore(element, ...payload) {
     element.before(...payload)
@@ -67,15 +54,87 @@ export default class DocumentObjectModel {
     element.remove();
   }
 
+  // REMOVE
+
+  removeNode(element) {
+    element.remove();
+  }
+
+  // GET ELEMENT
+
+  getElement(selector, target = this.document) {
+    return target.querySelector(selector);
+  }
+
+  getAllElements(selector, target = this.document) {
+    return target.querySelectorAll(selector);
+  }
+
+  getElementById(id, target = this.document) {
+    return target.getElementById(id);
+  }
+
   getRightNeighbor(item) {
     return item.nextElementSibling
   }
+
   getLeftNeighbor(item) {
     return item.previousElementSibling
   }
 
+  getParentNode(element) {
+    return element.parentNode;
+  }
+
+  getChildNodes(element) {
+    // childNodes returns child nodes
+    // (element nodes, text nodes, and comment nodes)
+    return element.childNodes;
+  }
+
+  getChildren(element) {
+    // children returns child elements
+    // (not text and comment nodes)
+    return element.children;
+  }
+
   getElementOffsetParent(element) {
     return element.offsetParent
+  }
+
+  // GET ELEMENT PROPS
+
+  getComputedStyle(element) {
+    return window.getComputedStyle(element);
+  }
+
+  getElementBCR(element) {
+    return element.getBoundingClientRect();
+  }
+
+  getElementOffsetLeft(element) {
+    return element?.offsetLeft;
+  }
+
+  getElementOffsetHeight(element) {
+    return element?.offsetHeight;
+  }
+
+  getElementOffsetWidth(element) {
+    return element?.offsetWidth;
+  }
+
+  getElementOffsetTop(element) {
+    return element?.offsetTop;
+  }
+
+  getElementOffsetBottom(element) {
+    // BUG ?
+    return element.offsetTop + element.offsetHeight || undefined;
+  }
+
+  getElementTagName(element) {
+    return element.tagName;
   }
 
   getDataId(item) { // (pages)
@@ -105,6 +164,8 @@ export default class DocumentObjectModel {
     element.getAttribute(selector);
   }
 
+  // SET ATTRIBUTES
+
   setAttribute(element, selector, value) {
     if (!element || !selector) {
       this._debugMode && this._debugToggler._DOM && console.warn('setAttribute() must have 2 params');
@@ -132,6 +193,19 @@ export default class DocumentObjectModel {
     this._debugMode && this._debugToggler._DOM && console.log(`you're really sure ${selector} is a selector?`)
   }
 
+  setStyles(element, styles) {
+    // styles is object
+    Object.entries(styles)
+      .forEach(([key, value]) =>
+        element.style[key] = value);
+  }
+
+  addClasses(element, ...cls) {
+    element.classList.add(...cls);
+  }
+
+  // REMOVE ATTRIBUTES
+
   removeAttribute(element, selector) {
     element.removeAttribute(selector);
   }
@@ -142,6 +216,10 @@ export default class DocumentObjectModel {
     }
   }
 
+  removeClasses(element, ...cls) {
+    element.classList.remove(...cls);
+  }
+
   removeAllClasses(element) {
     element.classList = '';
   }
@@ -150,53 +228,12 @@ export default class DocumentObjectModel {
     element.style = '';
   }
 
-  getComputedStyle(element) {
-    return window.getComputedStyle(element);
-  }
-
-  // CHECK
-
-  getElementTagName(element) {
-    return element.tagName;
-  }
-
-  isDocumentBody(element) {
-    return element.tagName === 'BODY';
-  }
-
-  isTextNode(element) {
-    return element.nodeType === Node.TEXT_NODE;
-  }
-  isElementNode(element) {
-    return element.nodeType === Node.ELEMENT_NODE;
-  }
-
-
-
-
-
-  // helpers
-
-  getParentNode(element) {
-    return element.parentNode;
-  }
-
-  getChildNodes(element) {
-    // childNodes returns child nodes
-    // (element nodes, text nodes, and comment nodes)
-    return element.childNodes;
-  }
-
-  getChildren(element) {
-    // children returns child elements
-    // (not text and comment nodes)
-    return element.children;
-  }
+  // GET/SET ELEMENT CONTENT
 
   getInnerHTML(selector) {
 
     if (typeof selector === 'string') {
-      const source = this._DOM.querySelector(selector);
+      const source = this.document.querySelector(selector);
       if (source) {
         return source.innerHTML;
       }
@@ -208,7 +245,7 @@ export default class DocumentObjectModel {
   setInnerHTML(selector, html) {
 
     if (typeof selector === 'string') {
-      const source = this._DOM.querySelector(selector);
+      const source = this.document.querySelector(selector);
       if (source) {
         source.innerHTML = html;
       }
@@ -217,54 +254,22 @@ export default class DocumentObjectModel {
     selector.innerHTML = html;
   }
 
-  setStyles(element, styles) {
-    // styles is object
-    Object.entries(styles)
-      .forEach(([key, value]) =>
-        element.style[key] = value);
+  // CHECK
+
+  isDocumentBody(element) {
+    return element.tagName === 'BODY';
   }
 
-  // CREATE ELEMENTS
-
-  createElement(selector) {
-    return this._DOM.createElement(selector);
+  isTextNode(element) {
+    return element.nodeType === Node.TEXT_NODE;
   }
 
-
-
-
-
-
-
-
-
-  // PAGES
-
-
-
-  getElementBCR(element) {
-    return element.getBoundingClientRect();
+  isElementNode(element) {
+    return element.nodeType === Node.ELEMENT_NODE;
   }
 
-  getElementOffsetLeft(element) {
-    return element?.offsetLeft;
-  }
-
-  getElementOffsetHeight(element) {
-    return element?.offsetHeight;
-  }
-
-  getElementOffsetWidth(element) {
-    return element?.offsetWidth;
-  }
-
-  getElementOffsetTop(element) {
-    return element?.offsetTop;
-  }
-
-  getElementOffsetBottom(element) {
-    // BUG ?
-    return element?.offsetTop + element?.offsetHeight || undefined;
+  isElementHasClass(element, cl) {
+    return element.classList.contains(cl);
   }
 
 }
