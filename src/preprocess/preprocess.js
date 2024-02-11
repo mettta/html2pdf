b4b7cb601b0ebc1543b3a760c875d7387789dee7
Maryna Balioura <mettta@gmail.com>
2024-02-11 17:16:01 +0100

    Preprocess: handling <object> preloading

--- src/preprocess/preprocess.js
+++ src/preprocess/preprocess.js
@@ -5,6 +5,7 @@ export default class Preprocess {
   }
 
   run() {
+
     let objects = [...document.querySelectorAll('object')];
     this._debugMode && console.log(objects);
 
@@ -12,12 +13,12 @@ export default class Preprocess {
 
     objects.forEach((object) => {
       if (object.contentDocument.readyState === 'complete') {
-        this._debugMode && console.log('I was loaded', object);
+        this._debugMode && console.log('I was loaded', object.clientHeight, object.clientWidth, object);
         return;
       }
       const promise = new Promise(resolve => {
         object.addEventListener("load", (event) => {
-          this._debugMode && console.log("EVENT: object load", object);
+          this._debugMode && console.log("EVENT: object load", object.clientHeight, object.clientWidth, object);
           resolve();
         });
       });
