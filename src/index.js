5e35f32547410b433d8e0c2ac342897dbd08b3e4
Maryna Balioura <mettta@gmail.com>
2024-02-17 13:42:45 +0100

    rename App.render

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import HTML2PDF4DOC from './app';
+import App from './app';
 
 const HTML2PDF4DOC_VERSION = '0.0.1';
 console.info(`HTML2PDF4DOC v${HTML2PDF4DOC_VERSION}`);
 
-new HTML2PDF4DOC(document.currentScript.dataset).render();
+new App(document.currentScript.dataset).render();
