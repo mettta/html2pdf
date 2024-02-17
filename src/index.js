037e7695e0a41db7b28db1e990dc2e0d357bc580
Maryna Balioura <mettta@gmail.com>
2024-02-17 20:05:52 +0100

    🏁 Add options to manually call the exported initiating function or automatically run the script.
    Closes #80

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import App from './app';
 const HTML2PDF4DOC_VERSION = '0.0.1';
 console.info(`HTML2PDF4DOC v${HTML2PDF4DOC_VERSION}`);
 
-// new App(document.currentScript.dataset).render();
-
-console.log('🏁 index.js');
 const dataset = document.currentScript.dataset;
-console.log('🏁 dataset', dataset);
 const app = new App(dataset);
 
+const isManualInit = dataset.init === "manual";
+isManualInit && console.info(`HTML2PDF4DOC in manual initialization mode`);
+
+!isManualInit && app.render();
+
 export function init() {
-  console.log('🏁🏁🏁 init');
-  app.render();
+  isManualInit && app.render();
 }
