ebaee37b1f27267ee12c45428629b7720e822c2e
Maryna Balioura <mettta@gmail.com>
2024-02-20 04:18:41 +0100

    test/end2end: add 2000_splitters: complex text block

--- test/end2end/2000_splitters/2033_split_complex_text_block/test_case.py
+++ test/end2end/2000_splitters/2033_split_complex_text_block/test_case.py
@@ -5,9 +5,10 @@ from seleniumbase import BaseCase
 from test.end2end.helpers.helper import Helper
 
 path_to_this_test_file_folder = os.path.dirname(os.path.abspath(__file__))
-index_html_file_url = (
-    "file:///" + os.path.join(path_to_this_test_file_folder, "index.html")
+case0_html_file_url = (
+    "file:///" + os.path.join(path_to_this_test_file_folder, "case0.html")
 )
+part_0 = '//html2pdf-complex-text-block//*[@data-child="0"]'
 
 
 class Test(BaseCase):
@@ -16,5 +17,6 @@ class Test(BaseCase):
         self.helper = Helper(self)
 
     def test_001(self):
-        self.helper.do_open(index_html_file_url)
+        self.helper.do_open(case0_html_file_url)
         self.helper.assert_html2pdf_success()
+        self.helper.assert_element_on_the_page(part_0, 1, True)
