b54a5329f09c2f375aa47770b691720cf0fa5e3e
Maryna Balioura <mettta@gmail.com>
2025-01-05 21:20:21 +0100

    test: updating tests for list splitting

--- test/end2end/2000_splitters/2040_list_split/test_case.py
+++ test/end2end/2000_splitters/2040_list_split/test_case.py
@@ -6,9 +6,7 @@ from seleniumbase import BaseCase
 from test.end2end.helpers.helper import Helper
 
 path_to_this_test_file_folder = os.path.dirname(os.path.abspath(__file__))
-index_html_file_url = (
-    "file:///" + os.path.join(path_to_this_test_file_folder, "index.html")
-)
+
 case1_html_file_url = (
     "file:///" + os.path.join(path_to_this_test_file_folder, "case1.html")
 )
@@ -24,6 +22,14 @@ case4_html_file_url = (
 case5_html_file_url = (
     "file:///" + os.path.join(path_to_this_test_file_folder, "case5.html")
 )
+case6_html_file_url = (
+    "file:///" + os.path.join(path_to_this_test_file_folder, "case6.html")
+)
+
+point_element = '//b[@class="point_element"]'
+line1 = '//b[@class="li1"]'
+line2 = '//b[@class="li2"]'
+line3 = '//b[@class="li3"]'
 
 
 # _T033_ListSplit
@@ -35,19 +41,36 @@ class Test(BaseCase):
     def test_01(self):
         self.helper.do_open(case1_html_file_url)
         self.helper.assert_document_has_pages(2)
+        self.helper.assert_element_on_the_page(line1, 2)
+        self.helper.assert_element_on_the_page(line2, 2)
+        self.helper.assert_element_on_the_page(line3, 2)
 
     def test_02(self):
         self.helper.do_open(case2_html_file_url)
         self.helper.assert_document_has_pages(2)
+        self.helper.assert_element_on_the_page(line1, 1)
+        self.helper.assert_element_on_the_page(line2, 2)
+        self.helper.assert_element_on_the_page(line3, 2)
 
     def test_03(self):
         self.helper.do_open(case3_html_file_url)
-        self.helper.assert_document_has_pages(1)
+        self.helper.assert_document_has_pages(2)
+        self.helper.assert_element_on_the_page(line1, 1)
+        self.helper.assert_element_on_the_page(line2, 1)
+        self.helper.assert_element_on_the_page(line3, 2)
 
     def test_04(self):
         self.helper.do_open(case4_html_file_url)
         self.helper.assert_document_has_pages(1)
+        self.helper.assert_element_on_the_page(line1, 1)
+        self.helper.assert_element_on_the_page(line2, 1)
+        self.helper.assert_element_on_the_page(line3, 1)
 
     def test_05(self):
         self.helper.do_open(case5_html_file_url)
         self.helper.assert_document_has_pages(2)
+
+    def test_06(self):
+        self.helper.do_open(case6_html_file_url)
+        self.helper.assert_document_has_pages(2)
+        self.helper.assert_element_on_the_page(point_element, 2)
