9912b7fbd8ed1297089d2dcc48a953eaea257ca9
Maryna Balioura <mettta@gmail.com>
2025-09-10 15:08:05 +0200

    test(Table/split): accurate height budget calculation; without a closing label in the last part.

--- test/end2end/2000_splitters/2080_table/test_case.py
+++ test/end2end/2000_splitters/2080_table/test_case.py
@@ -23,6 +23,15 @@ case5_html_file_url = (
 case6_html_file_url = (
     "file:///" + os.path.join(path_to_this_test_file_folder, "case6.html")
 )
+case10_html_file_url = (
+    "file:///" + os.path.join(path_to_this_test_file_folder, "case10.html")
+)
+case11_html_file_url = (
+    "file:///" + os.path.join(path_to_this_test_file_folder, "case10.html")
+)
+case12_html_file_url = (
+    "file:///" + os.path.join(path_to_this_test_file_folder, "case10.html")
+)
 
 
 class Test(BaseCase):
@@ -172,17 +181,77 @@ class Test(BaseCase):
         # But the second row contains a large unbreakable object.
         # It is expected that it will be transformed, and the second row
         # will occupy the entire 2nd page.
-        # TODO: Not implemented: #unbreakableOversized
-        # on the first and last row of the table the element will be scaled
-        # with a smaller coefficient than in the middle (due to break service
-        # labels - there are probably none at the beginning and end).
         self.helper.do_open(case6_html_file_url)
-        # TODO: Not implemented: #unbreakableOversized: should has 3 page
         self.helper.assert_document_has_pages(2)
         # 1
         self.helper.assert_element_on_the_page('//*[@data-testid="B0"]', 1)
         self.helper.assert_element_on_the_page('//*[@data-testid="B5"]', 1)
         # 2
         self.helper.assert_element_on_the_page('//*[@data-testid="box"]', 2)
-        # TODO: Not implemented: #unbreakableOversized: should be pushed to 3rd page
         self.helper.assert_element_on_the_page('//*[@data-testid="closer"]', 2)
+
+    # TODO: Not implemented: #unbreakableOversized
+        # on the first and last row of the table the element will be scaled
+        # with a smaller coefficient than in the middle (due to break service
+        # labels - there are probably none at the beginning and end).
+
+    def test_10(self):
+        # 3 pages.
+
+        # Case on the 1st page:
+        # * Accurate height budget calculation.
+        # if (filler's height = 99px [Chrome] / 98px [FF])
+        # => 1st page has 5 lines
+        # else if (filler's height +2px [Chrome] / +1px [FF]) -> 4 lines on 1st page
+        # * use 98 in the test
+
+        # Case on the last (3rd) page:
+        # * Without a closing label in the last part.
+        # The label “(table continues on the next page)” does not appear on the
+        # last part of the table and is not included in the budget calculations.
+        # Therefore, the last blue row B5 will not be moved to the 4th page,
+        # but will occupy this place at the end of the 3rd page.
+
+        self.helper.do_open(case10_html_file_url)
+        self.helper.assert_document_has_pages(3)
+        # 1
+        self.helper.assert_element_on_the_page('//*[@data-testid="R_0"]', 1)
+        self.helper.assert_element_on_the_page('//*[@data-testid="R_4"]', 1)
+        self.helper.assert_element_on_the_page('//*[@data-testid="R0"]', 1)
+        self.helper.assert_element_on_the_page('//*[@data-testid="R4"]', 1)
+        # 2
+        self.helper.assert_element_on_the_page('//*[@data-testid="R_5"]', 2)
+        self.helper.assert_element_on_the_page('//*[@data-testid="R5"]', 2)
+        self.helper.assert_element_on_the_page('//*[@data-testid="G4"]', 2)
+        self.helper.assert_element_on_the_page('//*[@data-testid="G_4"]', 2)
+        # 3
+        self.helper.assert_element_on_the_page('//*[@data-testid="G_5"]', 3)
+        self.helper.assert_element_on_the_page('//*[@data-testid="G5"]', 3)
+        # The last line B5 is here:
+        self.helper.assert_element_on_the_page('//*[@data-testid="B5"]', 3)
+
+    def test_11(self):
+        # 3 pages.
+        self.helper.do_open(case11_html_file_url)
+        self.helper.assert_document_has_pages(3)
+        # 3
+        self.helper.assert_element_on_the_page('//*[@data-testid="G_5"]', 3)
+        self.helper.assert_element_on_the_page('//*[@data-testid="G5"]', 3)
+        self.helper.assert_element_on_the_page('//*[@data-testid="B4"]', 3)
+        # The last TR "A" (1 line) is here:
+        self.helper.assert_element_on_the_page('//*[@data-testid="A_0"]', 3)
+        self.helper.assert_element_on_the_page('//*[@data-testid="A0"]', 3)
+
+    def test_12(self):
+        # 3 pages.
+        self.helper.do_open(case12_html_file_url)
+        self.helper.assert_document_has_pages(4)
+        # 3
+        self.helper.assert_element_on_the_page('//*[@data-testid="G_5"]', 3)
+        self.helper.assert_element_on_the_page('//*[@data-testid="G5"]', 3)
+        self.helper.assert_element_on_the_page('//*[@data-testid="B4"]', 3)
+        # 4
+        # The last TR "A" (4 lines) is here in its entirety:
+        self.helper.assert_element_on_the_page('//*[@data-testid="A_0"]', 3)
+        self.helper.assert_element_on_the_page('//*[@data-testid="A0"]', 3)
+        self.helper.assert_element_on_the_page('//*[@data-testid="A3"]', 3)
